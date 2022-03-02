889119a7ddec7fa1b931e0a31846d0e40832a45c
Eduardo Chapa <eduardo.chapa@gmail.com>
2022-03-02 13:03:05 -0600

    Added On/Off Toggle

--- twitter_autoload.js
+++ twitter_autoload.js
@@ -1,9 +1,10 @@
 var target = document.body;
 var scrollPos = window.scrollY;
+var status = "enabled";
 
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        if (scrollPos === 0 && mutation.target.className === "css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0")
+        if (status == "enabled" && scrollPos === 0 && mutation.target.className === "css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0")
             mutation.target.click();
     });
 });
@@ -14,6 +15,12 @@ observer.observe(target, config);
 
 document.addEventListener('scroll', function(e) {
     scrollPos = window.scrollY;
-    if (window.scrollY === 0)
+    if (status == "enabled" && window.scrollY === 0)
         document.getElementsByClassName('css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0')[0].click();
 });
+
+chrome.storage.onChanged.addListener(function (changes) {
+    status = changes.status.newValue;
+    if (status == "enabled" && window.scrollY === 0)
+        document.getElementsByClassName('css-901oao css-bfa6kz r-1kihuf0 r-jwli3a r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0')[0].click();
+})
